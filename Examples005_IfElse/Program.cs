5ea846411638bd40c1c708ad1014cceb4758318b
Tonatos10 <tonatos10@yandex.ru>
2022-11-22 01:16:18 +0300

    Моя пятая программа на С#

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Моя пятая программа на C#
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
