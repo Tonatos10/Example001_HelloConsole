59926b218d58b05a9106180382b062d044baa2af
Tonatos10 <tonatos10@yandex.ru>
2022-11-22 17:02:27 +0300

    После ToLower значение следует писать с маленькой буквы, тогда все работает!

--- Examples005_IfElse/Program.cs
+++ Examples005_IfElse/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "Маша") //ToLower() не работает, хз почему!
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
