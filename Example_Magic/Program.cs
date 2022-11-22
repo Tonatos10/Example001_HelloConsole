e8dcc1444d10218b58c04297eb27f0e010d071be
Tonatos10 <tonatos10@yandex.ru>
2022-11-22 22:17:39 +0300

    The largest number1

--- Example_Magic/Program.cs
+++ Example_Magic/Program.cs
@@ -1,20 +1,22 @@
 ﻿// My seven programm on C#
 Console.Clear();
-int xa = 40;
-int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 80;
-int yc = 30;
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
+
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
-//Console.SetCursorPosition(xc, yc);
-//Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
 int x = xa, y = xb;
+
 int count = 0;
-while(count < 100);
+
+while (count < 100);
 {
     int what = new Random().Next(0, 3); //Почему мы берем именно от 0, до 3?
     if (what == 0)
@@ -32,6 +34,8 @@ while(count < 100);
         x = (x + xc) / 2;
         y = (y + yc) / 2;
     }
+    
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
+    count++;
 }
